9ce6200da341e125a06cf2c38b4e4e995ce8255c
Akhil Kumar <akhil.kumar@quincus.com>
2020-06-28 16:27:44 +0530

    throw_pickup_noLink_done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,262 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
+import useWebSocket from 'react-use-websocket';
 import './App.css';
+import './card.css'
+
 
 function App() {
-  return (
+
+  // const [playerNumber, setPlayerNumber] = useState(0);
+  // const [playerNum, setPlayerNum] = useState(null);
+  // const [count, setCount] = useState(0);
+  
+  const [my_cards, setMy_cards] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [throwCards, setThrowCards] = useState(null);
+  const [playerNUmmm,setplayerNUmmm]=useState(null);
+  const socket = useRef(null);
+  // const [socket, setsocket] = useState(new WebSocket('ws://localhost:6789'));
+  // const socket = new WebSocket('ws://localhost:6789');
+
+
+  let playerNumber=0;
+  let playerNum=null;
+  // let count=0;
+  
+  // let url = window.location.hostName;
+  // let socket=null;
+  // let getSocket=(socket)=>{
+  //   return socket;
+  // }
+
+  // useEffect(() => {
+  //   const socket = new WebSocket(`ws://${window.location.href.split('//')[1].split(':')[0]}:1234`)
+  //   return () =>  getSocket(socket);
+  // }, []);
+
+  useEffect(() => {
+    console.log('creating one time socket');
+    socket.current = new WebSocket(`ws://${window.location.href.split('//')[1].split(':')[0]}:1234`);
+    socket.current.onopen = () => console.log("ws opened");
+    
+    socket.current.addEventListener('message', function (event) {
+      // console.log('Message from server ', event.data);
+    if(JSON.parse(event.data).type=="users")
+      {playerNumber = JSON.parse(event.data).count;
+    console.log("client is listening playerNumber playerNumber",playerNumber);
+    playerNum=`player ${playerNumber}` ;
+    setplayerNUmmm(playerNum);
+    }},{once: true})
+
+    socket.current.onclose = () => console.log("ws closed");
+}, []);
+
+  useEffect(() => {
+    // const socket = new Websocket.current('ws://localhost:6789');
+    // console.log("window.location.hostName window.location.hostName",window.location.href.split('//')[1].split(':')[0]);
+    // const socket.current = getsocket.current();
+    //  const socket.current = new Websocket.current(`ws://${window.location.href.split('//')[1].split(':')[0]}:1234`);
+    if(socket.current!=null && playerNUmmm!=null){
+    socket.current.addEventListener('open', function (event) {
+      console.log("client is listening ");
+    });
+            
+    // if(throwCards!=true && throwCards==null){
+  
+  socket.current.addEventListener('message', function (event) {
+    let card_status;
+    // console.log("------------------->",event.data.split("Message from server")[0]);
+    const all_cards = JSON.parse(event.data.split("Message from server")[0]);
+    // console.log("playerNum playerNum playerNum",playerNUmmm)
+    if(event.data.split("Message from server")[0].includes("player")){
+      setMy_cards([])
+      const my_card = Object.entries(all_cards).filter(([key,val])=>{
+        console.log("player ",key ,"its card",val);
+        return key==playerNUmmm
+      }
+      
+      );
+      // console.log("my_card my_card my_card",my_card);
+      my_card[0][1]["total_cards"].map((card)=>{
+        card_status = {"card": card, "isSelected": false};
+        // setMy_cards( [card_status])
+        console.log("my cardssssssss======>",card);
+        setMy_cards(spreadSheetData => [...spreadSheetData, card_status])
+        // setMy_cards({my_cards,...card_status})
+      })
+      // setSelectedCards({})
+      // debugger;
+    }
+  }
+  )
+
+
+  if(playerNUmmm!=null){
+    // console.log("sending messsage to socket srver");
+    // socket.current.addEventListener('open', function (event) {
+      socket.current.send(`{"action": "plus","playerNumber":"${playerNUmmm}","userType":"user"}`);
+  // });
+
+  } 
+// }
+
+  // if((throwCards==true || throwCards==false) && playerNUmmm!=null){
+    
+  // }
+}
+
+  
+  }, [playerNUmmm])
+
+
+  let toggleCardSelection=(cardall,cardToToggleStatus)=>{
+    let isSelectedCard=false;
+    let toggleStatus={};
+      let updatedCards=cardall.filter((ele)=>
+      {
+        // console.log("ele.cardele.card ele.card ele.card ele.cardklfjdkfs",cardToToggleStatus,ele.card);
+        if(ele.card==cardToToggleStatus)
+        { 
+          // console.log("pakdaa gaya");
+        // console.log("card to be removed",ele,cardToToggleStatus);
+          isSelectedCard = !ele.isSelected;} 
+        return ele.card!==cardToToggleStatus;
+      }
+  
+      )
+      
+      // console.log("updatedCards updatedCards updatedCards",updatedCards);
+      toggleStatus = {"card": cardToToggleStatus,"isSelected":isSelectedCard};
+      // debugger;
+      setMy_cards([...updatedCards,toggleStatus] )
+     
+  
+  }
+
+
+useEffect(() => {
+  if(my_cards!=null){
+    // console.log("muy scards state====>",my_cards);
+  }
+}, [my_cards])
+    
+// let findDesign=(ele)=>{
+//   let  alpha = ele.split("");
+//   if(alpha.length!==3)
+//   {
+//     console.log("--------=========ffffffff=fds===>",alpha[1]);
+//     if (alpha[1]==='C')
+//   return "clubs"
+//   else if (alpha[1]==='D')
+//   return "diams"
+//   else if (alpha[1]==='H')
+//   return "hearts"
+//   else if (alpha[1]==='S')
+//   return "spades"
+// }else{
+//     if (alpha[2]==='C')
+//   return "clubs"
+//   else if (alpha[2]==='D')
+//   return "diams"
+//   else if (alpha[2]==='H')
+//   return "hearts"
+//   else if (alpha[2]==='S')
+//   return "spades"
+//   }
+// }
+
+let getIconContent=(alpha,index,isIcon)=>{
+  if (alpha[index]==='C')
+  return isIcon ? "♣": "clubs";
+  else if (alpha[index]==='D')
+  return isIcon ? "♦" : "diams";
+  else if (alpha[index]==='H')
+  return isIcon ? "♥" : "hearts";
+  else if (alpha[index]==='S')
+  return isIcon ? "♠" : "spades";
+}
+
+let findDesign=(ele,isIcon)=>{
+  let  alpha = ele.split("");
+  if(alpha.length!==3)
+    return getIconContent(alpha,1,isIcon)
+  else
+  return getIconContent(alpha,2,isIcon);
+}
+
+let findno=(ele)=>{
+  let alpha=ele.split("");
+  if(alpha.length!==3)
+  return alpha[0];
+  else 
+  return [alpha[0],alpha[1]].join("");
+
+}
+
+let pickCards=()=>{
+  console.log("pick_card_request_send of player no ",playerNUmmm);
+  socket.current.send(`{"action": "pick_cards","playerNumber":"${playerNUmmm}","userType":"user"}`);
+}
+
+let throwCardsfn=()=>{
+  let create_throwObject={};
+  let card_to_be_thrown = my_cards.filter((ele)=>
+  ele.isSelected!==false
+)
+
+card_to_be_thrown.map((ele)=>{   //deleting other property
+  // console.log(ele.isSelected)
+  delete ele.isSelected;
+})
+// socket.current.addEventListener('open', function (event) {
+  // console.log("call to car_to_be_thrown",card_to_be_thrown);
+  create_throwObject = {...{"action": "throw_card","playerNumber":`${playerNUmmm}`,"userType":"user"},...{"thrown_cards":card_to_be_thrown}}
+  console.log("call to nhi ho raha????",JSON.stringify(create_throwObject));
+  socket.current.send(JSON.stringify(create_throwObject) );
+// }); "${JSON.stringify({"thrown_cards":car_to_be_thrown})}
+}
+
+
+
+
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <div class="playingCards fourColours rotateHand">
+<ul className="hand">
+      {my_cards ?my_cards.map((ele)=>{return !ele.isSelected ?  (
+
+
+        <li onClick={()=>toggleCardSelection(my_cards,ele.card)}>
+        <a className={`card rank-${findno(ele.card).isInteger ? findno(ele.card): findno(ele.card).toLowerCase()} ${findDesign(ele.card,false)}`} >
+        <span class="rank">{findno(ele.card)}</span>{findDesign(ele.card,true)}<span class="suit"></span>
+                {/* <span class="rank">7</span>
+                <span class="suit">&diams;</span> */}
+            </a>
+            {/* <div className={`card rank-${findno(ele).isInteger ? findno(ele): findno(ele).toLowerCase()} ${findDesign(ele)}`}><span class="rank">{findno(ele)}</span>{findDesign2(ele)}<span class="suit"></span></div> */}
+        
+        </li>
+		
+      ): (
+      <li onClick={()=>toggleCardSelection(my_cards,ele.card)}>
+        <a style={{bottom: "1em"}} className={`card rank-${findno(ele.card).isInteger ? findno(ele.card): findno(ele.card).toLowerCase()} ${findDesign(ele.card,false)}`} >
+        <span class="rank">{findno(ele.card)}</span>{findDesign(ele.card,true)}<span class="suit"></span>
+                {/* <span class="rank">7</span>
+                <span class="suit">&diams;</span> */}
+            </a>
+            {/* <div className={`card rank-${findno(ele).isInteger ? findno(ele): findno(ele).toLowerCase()} ${findDesign(ele)}`}><span class="rank">{findno(ele)}</span>{findDesign2(ele)}<span class="suit"></span></div> */}
+        
+        </li>
+        )
+      }): null}
+      </ul>
+		</div>
+    {my_cards.length!=0?(
+    <div className="a">
+    <button onClick={()=>throwCardsfn()}>Throw</button>
+    <button onClick={()=>pickCards()}>Pick cards</button>
+    </div>
+    ):null}
     </div>
   );
 }
---
+++ src/web_socket_server.py
@@ -0,0 +1,109 @@
+import asyncio
+import json
+import logging
+import websockets
+from bluff_python import bluff
+
+logging.basicConfig()
+
+STATE = {"value": 0}
+
+USERS = set()
+
+
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    print("=========> {}".format(websocket))
+    await notify_users()
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+async def send_cards_to_client(card_to_be_distri_to_players):
+	try:
+	    await asyncio.wait([user.send(json.dumps(card_to_be_distri_to_players)) for user in USERS])
+	finally:
+		print("hello2")
+
+async def throw_cards(data,bluffw):
+	bluffw.last_player_throw_record = data["playerNumber"];
+    print("throw card calledd")
+    for k in data["thrown_cards"]:
+        print("keyyy {}".format(k))
+    result=bluffw.throw_cards(data["playerNumber"],data["thrown_cards"])
+    await send_cards_to_client(result)
+    print("result from tx bluff {}".format(result))   
+	
+async def pick_cards(data,bluffw):
+    result=bluffw.pick_cards_from_mat(data["playerNumber"])
+    print("pick_cards card calledd {}".format(result))
+    await send_cards_to_client(result)
+
+
+async def counter(websocket, path):
+    global bluffw
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            print("messagggggeeeeeeeeeeee=====> {}".format(data))
+            if data["action"] == "minus":
+                STATE["value"] -= 1
+                await notify_state()
+            elif data["action"] == "plus" and data["userType"]=="admin":
+                bluffw = bluff(len(USERS))
+                STATE["value"] += 1
+                await distribute_cards(bluffw)
+                await notify_state()
+            elif data["action"] == "throw_card":
+                await throw_cards(data,bluffw)
+            elif data["action"] == "pick_cards":
+                await pick_cards(data,bluffw)
+
+            else:
+                logging.error("unsupported event: {}".format(data))
+                #print("data------------> {}".format(data))
+    finally:
+        print("hello")
+        #await unregister(websocket)
+
+
+		#await unregister(websocket)
+
+
+	
+async def distribute_cards(bluffw):
+    no_of_deck=1
+    bluffw.shuffle_cards_fn(no_of_deck)
+    get_card_distribution_to_player = bluffw.card_distribution_to_player()
+    await send_cards_to_client(get_card_distribution_to_player)
+
+
+start_server = websockets.serve(counter, port=1234)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
